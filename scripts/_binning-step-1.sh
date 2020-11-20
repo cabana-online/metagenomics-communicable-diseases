44270426ea1b6267aa13d83710c3eb0ef54efe09
Alejandro Madrigal Leiva <me@alemadlei.tech>
2020-11-20 11:19:55 -0600

    Clustering and binning step completed.

--- scripts/_binning-step-1.sh
+++ scripts/_binning-step-1.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Moves to the work directory.
-pushd /home/cabana/CABANA/06.Assembly
+pushd /home/cabana/CABANA/07.Binning
+
+# Clears the folder before starting work.
+rm *
 
 echo "Performing binning. This should take around 45 minutes."
 for i in $(ls ../03.CleanLib/*.CoupledReads.fa); do
-  NAME=$(basename $i .CoupledReads.fasta);
-  run_MaxBin.pl -contig ../06.Assembly/$NAME.contigs.fa -reads  ../03.CleanLib/$NAME.CoupledReads.fasta -out $NAME -thread 4 -plotmarker;
+  NAME=$(basename $i .CoupledReads.fa);
+  run_MaxBin.pl -contig ../06.Assembly/$NAME/$NAME.contigs.fa -reads  ../03.CleanLib/$NAME.CoupledReads.fa -out $NAME -thread 4 -plotmarker;
 done
+echo "Result files:"
 ls
+sleep 2
