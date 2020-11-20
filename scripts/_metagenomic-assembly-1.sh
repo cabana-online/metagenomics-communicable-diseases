ece2667155f15dd5f67f782589614d4ef7908438
Alejandro Madrigal Leiva <me@alemadlei.tech>
2020-11-20 08:55:27 -0600

    Metagenomic assembly complete.

--- scripts/_metagenomic-assembly-1.sh
+++ scripts/_metagenomic-assembly-1.sh
@@ -3,8 +3,8 @@
 # Moves to the work directory.
 pushd /home/cabana/CABANA/06.Assembly
 
-echo "Performing assembly. This should take around 60 minutes."
+echo "Performing assembly. This should take around 45 minutes."
 for i in $(ls ../03.CleanLib/*.CoupledReads.fa); do
-  megahit --12 $i -t 4  --out-prefix $(basename $i .CoupledReads.fasta) --min-contig-len 500 -o ./$(basename $i .CoupledReads.fasta);
+  megahit --12 $i -t 4  --out-prefix $(basename $i .CoupledReads.fa) --min-contig-len 500 -o ./$(basename $i .CoupledReads.fa);
 done
 ls
