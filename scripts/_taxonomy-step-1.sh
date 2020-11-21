ed7e72e404f9222607519b5fcea26bd6927b1fce
Alejandro Madrigal Leiva <me@alemadlei.tech>
2020-11-21 09:51:16 -0600

    Adding metaphlan2krona and ktImportText to the taxonomy step.

--- scripts/_taxonomy-step-1.sh
+++ scripts/_taxonomy-step-1.sh
@@ -9,10 +9,23 @@ rm ../03.CleanLib/*bowtie2*
 
 echo "Performing taxonomy. This should take around 45 minutes."
 for i in $(ls ../03.CleanLib/*.CoupledReads.fa); do
+
+  # Gets the name of the item to process.
   NAME=$(basename $i .CoupledReads.fa);
+
   echo "Metaphlanning $NAME"
   metaphlan2.py ../03.CleanLib/$NAME.CoupledReads.fa --input_type fasta --nproc 4 > ./${NAME}_profile.txt;
-  echo "Displaying resulting file"
+  echo "Displaying resulting file ${NAME}_profile.txt".
+  cat ${NAME}_profile.txt
+
+  echo "Converting to krona format".
+  metaphlan2krona.py --profile ${NAME}_profile.txt --krona ./${NAME}_Krona.txt
+  echo "Displaying resulting file ${NAME}_Krona.txt".
+  cat ${NAME}_Krona.txt
+
+  echo "Generating HTML plot at ${NAME}_Krona.html"
+  ktImportText ${NAME}_Krona.txt -o ${NAME}_Krona.html
+
 done
 
 popd
