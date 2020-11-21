8aa49d1f96e87312d4e6e87504ffa4143e1102f4
Alejandro Madrigal Leiva <me@alemadlei.tech>
2020-11-21 03:21:02 -0600

    Running metaphlan2.

--- scripts/_taxonomy-step-1.sh
+++ scripts/_taxonomy-step-1.sh
@@ -10,5 +10,8 @@ echo "Performing taxonomy. This should take around 45 minutes."
 for i in $(ls ../03.CleanLib/*.CoupledReads.fa); do
   NAME=$(basename $i .CoupledReads.fa);
   echo "Metaphlanning $NAME"
-  metaphlan2.py ../03.CleanLib/$NAME.CoupledReads.fa --input_type fasta --nproc 4 -o ./$NAME_profile.txt;
+  metaphlan2.py ../03.CleanLib/$NAME.CoupledReads.fa --input_type fasta --nproc 4 > ./$NAME_profile.txt;
+  echo "Displaying resulting file"
 done
+
+popd
