396b267bc473ff044bd3aa14a76010f610417f38
Alejandro Madrigal Leiva <me@alemadlei.tech>
2020-11-21 14:54:28 -0600

    Taxonomy step graphs.

--- scripts/_taxonomy-step-1.sh
+++ scripts/_taxonomy-step-1.sh
@@ -4,8 +4,8 @@
 pushd /home/cabana/CABANA/08.Taxonomy
 
 # Clears the folder before starting work.
-#rm -rf *
-#rm ../03.CleanLib/*bowtie2*
+rm -rf *
+rm ../03.CleanLib/*bowtie2*
 
 echo "Performing taxonomy. This should take around 45 minutes."
 for i in $(ls ../03.CleanLib/*.CoupledReads.fa); do
@@ -14,7 +14,7 @@ for i in $(ls ../03.CleanLib/*.CoupledReads.fa); do
   NAME=$(basename $i .CoupledReads.fa);
 
   echo "Metaphlanning $NAME"
-#  metaphlan2.py ../03.CleanLib/$NAME.CoupledReads.fa --input_type fasta --nproc 4 > ./${NAME}_profile.txt;
+  metaphlan2.py ../03.CleanLib/$NAME.CoupledReads.fa --input_type fasta --nproc 4 > ./${NAME}_profile.txt;
   echo "Displaying resulting file ${NAME}_profile.txt".
   cat ${NAME}_profile.txt
 
